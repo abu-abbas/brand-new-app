4ffb70823ec13254c1034a6af82d9be9d1adef47
abu-abbas <notificationfor.gitlabs@gmail.com>
2026-07-28 13:04:30 +0700

    feat(resizable): tambah komponen resizable bawaan shadcn-vue
    - Menambahkan ResizablePanelGroup, ResizablePanel, dan ResizableHandle berbasis reka-ui.
    - Menyediakan komponen UI resizable yang dapat disesuaikan sesuai spesifikasi shadcn-vue.

---
+++ resources/js/components/ui/resizable/ResizableHandle.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue';
+import type { SplitterResizeHandleEmits, SplitterResizeHandleProps } from 'reka-ui';
+import { reactiveOmit } from '@vueuse/core';
+import { GripVertical } from '@lucide/vue';
+import { SplitterResizeHandle, useForwardPropsEmits } from 'reka-ui';
+import { cn } from '@/lib/utils';
+
+const props = withDefaults(
+  defineProps<
+    SplitterResizeHandleProps & { class?: HTMLAttributes['class']; withHandle?: boolean }
+  >(),
+  {
+    withHandle: false,
+  },
+);
+const emits = defineEmits<SplitterResizeHandleEmits>();
+
+const delegatedProps = reactiveOmit(props, 'class', 'withHandle');
+const forwarded = useForwardPropsEmits(delegatedProps, emits);
+</script>
+
+<template>
+  <SplitterResizeHandle
+    v-bind="forwarded"
+    :class="
+      cn(
+        'relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90',
+        props.class,
+      )
+    "
+  >
+    <div
+      v-if="props.withHandle"
+      class="z-10 flex h-7 w-3.5 items-center justify-center rounded-xs border bg-border text-muted-foreground shadow-xs"
+    >
+      <GripVertical class="h-2.5 w-2.5" />
+    </div>
+  </SplitterResizeHandle>
+</template>
---
+++ resources/js/components/ui/resizable/ResizablePanel.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue';
+import type { SplitterPanelEmits, SplitterPanelProps } from 'reka-ui';
+import { reactiveOmit } from '@vueuse/core';
+import { SplitterPanel, useForwardPropsEmits } from 'reka-ui';
+
+const props = defineProps<SplitterPanelProps & { class?: HTMLAttributes['class'] }>();
+const emits = defineEmits<SplitterPanelEmits>();
+
+const delegatedProps = reactiveOmit(props, 'class');
+const forwarded = useForwardPropsEmits(delegatedProps, emits);
+</script>
+
+<template>
+  <SplitterPanel v-bind="forwarded" :class="props.class">
+    <slot />
+  </SplitterPanel>
+</template>
